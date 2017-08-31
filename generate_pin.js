c819a59b45bae434e23bccaecbed927ea849426e
Nathan Huesmann <nathan@chefd.com>
2017-08-31 10:43:00 -0700

    initial commit

---
+++ generate_pin.js
@@ -0,0 +1,50 @@
+const stringify = require('csv-stringify');
+const fs = require('fs');
+const order = require('./order.json');
+
+var columns = {};
+var pins = [];
+// TODO: How to get last PIN? Call apps server here? Or have this be a command line argument? Or add to order JSON?
+var lastPin = '12365470000000BH';
+
+const random = () => {
+  return Math.floor(1000000 + Math.random() * 9000000);
+};
+
+const incrementString = (string) => {
+  const toInt = +string + 1;
+  return ('0000000' + toInt).slice(-7);
+};
+
+const generate = (lastPinArg, quantity) => {
+  let counter = lastPinArg.slice(-9).slice(0, 7);
+
+  for (let i = 0; i < quantity; i++) {
+    counter = incrementString(counter);
+
+    let pin = `${random()}${counter}BH`;
+    if (!pins[i]) {
+      pins.push([pin]);
+    } else {
+      pins[i] = pins[i].concat([pin]);
+    }
+  };
+
+  lastPin = pins[pins.length - 1][0];
+};
+
+order.forEach((order, index) => {
+  generate(lastPin, order.quantity);
+  columns[`card${1 + index}`] = order.upc;
+});
+
+stringify(pins, {
+  columns,
+  header: true
+}, (err, output) => {
+  if (err) {
+    console.log(err);
+  }
+
+  fs.writeFileSync(`${__dirname}/pins.csv`, output);
+});
